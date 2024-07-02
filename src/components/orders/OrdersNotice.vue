9fc4d65c3bd25e51dbf4be17e461ca945d92d184
gastronome <1305573134@qq.com>
2024-07-02 11:25:13 +0800

    完善了服务须知

--- src/components/orders/OrdersNotice.vue
+++ src/components/orders/OrdersNotice.vue
@@ -8,7 +8,7 @@ const handleAgree = () => {
   if (isAgreed.value) {
     emit('agreementStatus', true)
   } else {
-    alert('请先同意此协议')
+    alert('请先认真阅读，服务须知')
   }
 }
 </script>
@@ -18,16 +18,18 @@ const handleAgree = () => {
     <h2>服务须知</h2>
     <p class="notice">
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;一、本服务仅提供广东农工商职业技术学院北校区师生。<br />
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;二、正常情况下我们会根据报修的时间顺序进行处理，请耐心等候工作人员跟进，如有其他情况请在公众号留言。<br />
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;三、如需【取消报修】，请在ITeam维修站公众号留言“宿舍号+姓名”。<br />
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;四、我们“ITeam基地-维修站”的所有成员，是本校计算机系各专业的同学们，本着一颗服务同学们的心，在实践中提升自己的专业技能，从不收取一分钱。<br />
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;五、我们不销售任何电子产品，如需要可给予购买意见。<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;二、在使用本服务之前，表示你已同意承担在处理过程中可能存在的一定风险。<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;三、我们“ITeam基地-维修站”的所有成员，本着一颗服务同学们的心，在实践中提升自己的专业技能，从不收取一分钱。<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;四、正常情况下我们会根据报修的时间顺序进行处理，请耐心等候工作人员与您联系。（我们不会以任何方式添加您的微信）<br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;五、我们不销售任何电子产品，不提供购买意见。<br />
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;六、在使用本服务之前，表示你已同意承担在处理过程中可能存在的风险。<br />
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;七、如有任何纠纷投诉请与本公众号取得联系。<br />
     </p>
     <div class="read-service">
-      <input type="checkbox" v-model="isAgreed" />
-      已阅读，并同意此协议
+      <label>
+        <input type="checkbox" v-model="isAgreed" />
+        已阅读，并了解其风险
+      </label>
     </div>
     <nut-button size="small" shape="round" type="info" @click="handleAgree"
       >同意须知
@@ -46,7 +48,7 @@ const handleAgree = () => {
     margin: 0 auto;
   }
 
-  .read-service {
+  .read-service label {
     cursor: pointer;
   }
 
--- src/views/orders/NewOrder.vue
+++ src/views/orders/NewOrder.vue
@@ -1,12 +1,18 @@
 <script setup>
 import { Agreement, EditOne, Tool } from '@icon-park/vue-next'
 import { ref } from 'vue'
+import OrdersNotice from '@/components/orders/OrdersNotice.vue'
 
 const current = ref(1)
 const onClickStep = (index) => {
   console.log(index)
+}
 
-  current.value = index
+const handleAgreementStatus = (status) => {
+  console.log('User agreement status:', status)
+  if (status) {
+    current.value = 2
+  }
 }
 </script>
 
@@ -34,25 +40,10 @@ const onClickStep = (index) => {
             </template>
           </nut-step>
         </nut-steps>
-        <div class="service-notice">
-          <h2>服务须知</h2>
-          <p class="notice">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;一、本服务仅提供广东农工商职业技术学院北校区师生。<br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;二、正常情况下我们会根据报修的时间顺序进行处理，请耐心等候工作人员跟进，如有其他情况请在公众号留言。<br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;三、如需【取消报修】，请在ITeam维修站公众号留言“宿舍号+姓名”。<br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;四、我们“ITeam基地-维修站”的所有成员，是本校计算机系各专业的同学们，本着一颗服务同学们的心，在实践中提升自己的专业技能，从不收取一分钱。<br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;五、我们不销售任何电子产品，如需要可给予购买意见。<br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;六、在使用本服务之前，表示你已同意承担在处理过程中可能存在的风险。<br />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;七、如有任何纠纷投诉请与本公众号取得联系。<br />
-          </p>
-          <div class="read-service">
-            <input type="checkbox" />
-            已阅读，并同意此协议
-          </div>
-          <nut-button size="small" shape="round" type="info"
-            >同意须知</nut-button
-          >
-        </div>
+        <orders-notice
+          v-if="current === 1"
+          @agreementStatus="handleAgreementStatus"
+        ></orders-notice>
       </div>
     </div>
   </div>
