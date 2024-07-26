8187009ed77814610f904a6d9b034dff0ae1047a
gastronome <1305573134@qq.com>
2024-07-27 00:16:38 +0800

    修改瑕疵

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,8 @@
 <script setup>
 import OrdersFooter from '@/components/orders/OrdersFooter.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 </script>
 
 <template>
@@ -7,6 +10,9 @@ import OrdersFooter from '@/components/orders/OrdersFooter.vue'
   <h1>找不到网页</h1>
   <div>
     <img src="@/assets/rs_logo.png" alt="" />
+    <nut-button @click="router.push('/')" size="large" type="danger">
+      点击跳转到报修页面
+    </nut-button>
   </div>
   <orders-footer></orders-footer>
 </template>
@@ -14,10 +20,14 @@ import OrdersFooter from '@/components/orders/OrdersFooter.vue'
 <style scoped>
 div {
   width: 50%;
-  margin: 100px auto;
+  margin: 50px auto 20px;
+  text-align: center;
   img {
     width: 100%;
     margin: 0 auto;
+  }
+  .nut-button--round {
+    margin-top: 35px;
   }
 }
 h1 {
