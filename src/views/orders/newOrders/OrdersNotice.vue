3551f94bf59ed8f704806a2cf8811e4b7d2daa90
gastronome <1305573134@qq.com>
2024-10-14 09:37:54 +0800

    优化公告下拉逻辑

--- src/views/orders/newOrders/OrdersNotice.vue
+++ src/views/orders/newOrders/OrdersNotice.vue
@@ -34,20 +34,22 @@ const show = ref(false)
   >
     <div class="wrapper">
       <div class="orders-notice" @click.stop>
-        <h2>服务须知</h2>
-        <p class="notice">
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;一、本服务仅提供广东农工商职业技术学院师生。<br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;二、在使用本服务之前，表示你已同意承担在处理过程中可能存在的一定风险。<br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;三、我们“ITeam基地-维修站”的所有成员，本着一颗服务同学们的心，在实践中提升自己的专业技能，
-          <span style="color: red">公益服务从不收取一分钱</span>。<br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;四、正常情况下，我们会根据报修的时间顺序进行维修，请耐心等候社团成员与您进行短信联系。（为避免纠纷，我们不会以任何方式添加您的微信）<br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;五、我们既不销售任何电子产品，也不供提任何购买意见。<br />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;六、如遇紧急情况，需要维修电脑可添加维修站QQ咨询群：790445318进行询问。<br />
-          <span style="color: red">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;七、如有任何纠纷或需进行投诉，请扫描下方二维码与本公众号取得联系！
-          </span>
-          <br />
-        </p>
+        <div class="text">
+          <h2 style="margin-top: 0">服务须知</h2>
+          <p class="notice">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;一、本服务仅提供广东农工商职业技术学院师生。<br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;二、在使用本服务之前，表示你已同意承担在处理过程中可能存在的一定风险。<br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;三、我们“ITeam基地-维修站”的所有成员，本着一颗服务同学们的心，在实践中提升自己的专业技能，
+            <span style="color: red">公益服务从不收取一分钱</span>。<br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;四、正常情况下，我们会根据报修的时间顺序进行维修，请耐心等候社团成员与您进行短信联系。（为避免纠纷，我们不会以任何方式添加您的微信）<br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;五、我们既不销售任何电子产品，也不供提任何购买意见。<br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;六、如遇紧急情况，需要维修电脑可添加维修站QQ咨询群：790445318进行询问。<br />
+            <span style="color: red">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;七、如有任何纠纷或需进行投诉，请扫描下方二维码与本公众号取得联系！
+            </span>
+            <br />
+          </p>
+        </div>
 
         <div class="read-service">
           <label>
@@ -90,25 +92,30 @@ const show = ref(false)
     text-align: center;
     background-color: white;
     width: 85%;
-    max-height: 80vh;
     margin: 0 auto;
     border-radius: 10px;
     padding: 20px;
-    overflow-y: auto;
-    -webkit-overflow-scrolling: touch; /* 使iOS设备上的滚动更加平滑 */
 
-    .notice {
-      text-align: left;
-      padding-bottom: 30px;
-      margin: 0 auto;
+    .text {
+      overflow-y: auto;
+      max-height: 50vh;
+      -webkit-overflow-scrolling: touch; /* 使iOS设备上的滚动更加平滑 */
+      .notice {
+        text-align: left;
+        padding-bottom: 10px;
+        margin: 0 auto;
+      }
     }
 
-    .read-service label {
-      cursor: pointer;
+    .read-service {
+      margin-top: 5px;
+      .read-service label {
+        cursor: pointer;
+      }
     }
 
     .nut-button {
-      margin: 5px 0 15px 0;
+      margin: 5px 0 0 0;
       width: 80%;
       height: 45px;
     }
