c0cbfedbd152fb4c729802f6725dc095dc9fc4e2
gastronome <1305573134@qq.com>
2024-10-21 23:03:41 +0800

    优化上传

--- src/views/tool/Tool.vue
+++ src/views/tool/Tool.vue
@@ -6,7 +6,114 @@ import { ref } from 'vue'
 
 const router = useRouter()
 
-const appList = ref(window.config.appList)
+const appList = ref([
+  {
+    title: '日常软件',
+    list: [
+      {
+        name: 'QQ',
+        url: 'https://im.qq.com/index/#downloadAnchor',
+        photo:
+          'https://qzonestyle.gtimg.cn/qzone/qzact/act/external/tiqq/logo.png'
+      },
+      {
+        name: '微信',
+        url: 'https://pc.weixin.qq.com/',
+        photo:
+          'https://res.wx.qq.com/t/wx_fed/base/portal/windows/res/assets/logo.ca85d20a32.svg'
+      },
+      {
+        name: '飞书',
+        url: 'https://www.feishu.cn/download',
+        photo:
+          'https://p1-hera.feishucdn.com/tos-cn-i-jbbdkfciu3/84a9f036fe2b44f99b899fff4beeb963~tplv-jbbdkfciu3-image:0:0.image'
+      },
+      {
+        name: '钉钉',
+        url: 'https://page.dingtalk.com/wow/z/dingtalk/simple/ddhomedownload#/',
+        photo:
+          'https://gw.alicdn.com/imgextra/i3/O1CN01eMicSg1GVD4uXMWGv_!!6000000000627-73-tps-32-32.ico'
+      },
+      {
+        name: 'Todesk',
+        url: 'https://www.todesk.com/download.html',
+        photo: 'https://www.todesk.com/dist/img/734933e.svg'
+      },
+      {
+        name: 'steam',
+        url: 'https://store.steampowered.com/about/',
+        photo: 'https://store.steampowered.com/favicon.ico'
+      },
+      {
+        name: 'chrome',
+        url: 'https://www.google.cn/intl/zh-CN/chrome/',
+        photo: 'https://www.google.cn/chrome/static/images/chrome-logo-m100.svg'
+      },
+      {
+        name: 'WeGame',
+        url: 'https://www.wegame.com.cn/client/',
+        photo:
+          'https://wegame.gtimg.com/g.55555-r.c4663/wegame-home/wegame-h5-share.f036f35a.jpg'
+      }
+    ]
+  },
+  {
+    title: '装机可用',
+    list: [
+      {
+        name: '图吧工具箱',
+        url: 'https://www.tbtool.cn/',
+        photo: 'https://www.tbtool.cn/resource/icon.ico'
+      },
+      {
+        name: 'Nvidia驱动',
+        url: 'https://www.nvidia.cn/drivers/lookup/',
+        photo: 'https://www.nvidia.cn/favicon.ico'
+      },
+      {
+        name: 'AMD驱动',
+        url: 'https://www.amd.com/zh-cn/support/download/drivers.html',
+        photo: 'https://www.amd.com/content/dam/code/images/favicon/favicon.ico'
+      },
+      {
+        name: 'geek卸载工具',
+        url: 'https://geekuninstaller.com/',
+        photo: 'https://geekuninstaller.com/assets/images/icon_geek_32.png'
+      }
+    ]
+  },
+  {
+    title: '软件开发',
+    list: [
+      {
+        name: 'VsCode',
+        url: 'https://code.visualstudio.com/docs/?dv=win64user',
+        photo: 'https://code.visualstudio.com/favicon.ico'
+      },
+      {
+        name: 'sublimetext',
+        url: 'https://www.sublime.com/',
+        photo: 'https://www.sublimehq.com/images/sublime_text.png'
+      },
+      {
+        name: 'IDEA',
+        url: 'https://www.jetbrains.com/idea/download/#section=windows',
+        photo: 'https://www.jetbrains.com/icon.svg?r=1234'
+      },
+      {
+        name: 'eclipse',
+        url: 'https://www.eclipse.org/downloads/',
+        photo:
+          'https://www.eclipse.org/downloads/assets/public/images/logo-eclipse.png'
+      },
+      {
+        name: 'nodejs',
+        url: 'https://nodejs.cn/download/',
+        photo: 'https://img.nodejs.cn/favicon.png'
+      }
+    ]
+  }
+])
 
 const back = () => {
   router.go(-1)
