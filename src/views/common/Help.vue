f48add3d049912827fcb5c9944cf7dd93383c28b
gastronome <1305573134@qq.com>
2024-10-21 13:55:47 +0800

    完善帮助页

--- src/views/common/Help.vue
+++ src/views/common/Help.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 const router = useRouter()
 const route = useRoute()
@@ -9,6 +10,14 @@ const onClick = () => {
   } else {
     router.push('/staff/profile')
   }
+}
+
+const activeName = ref(null)
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
 }
 </script>
 
@@ -22,55 +31,83 @@ const onClick = () => {
   </nut-sticky>
   <div class="helpPage">
     <h2 align="center">维修站报修系统操作说明</h2>
-    <p>1.首页，查看当前故障报修数量、个人信息、公告以及更多。</p>
-    <img src="@/assets/help/help1.jpg" width="250px" alt="" /><br />
-    <nut-divider dashed />
-    <p>2.点击按钮进来，点击“抢单”为接手处理此故障订单。</p>
-    <img src="@/assets/help/help2.jpg" width="250px" alt="" /><br />
-    <nut-divider dashed />
-    <p>3.点击上方“维修中”可以查看已接单</p>
-    <img src="@/assets/help/help3.jpg" width="250px" alt="" />
-    <img src="@/assets/help/help4.jpg" width="250px" alt="" /><br />
-    <nut-divider dashed />
-    <p>4.这里是自己接的单。维修完成后点击黄字”维修中“结束维修单。</p>
-    <img src="@/assets/help/help5.jpg" width="250px" alt="" /><br />
-    <nut-divider dashed />
-    <p>
-      5.1
-      如果有“拆机”单在修单前，需要点击“签订协议”，同时需要给笔记本细节进行拍照。机主签完名之后记得自己截图保存<br />
-      <span style="color: red">
-        如自己没有保存照片及截图，后期纠纷社员自己承担
-      </span>
+    <van-collapse v-model="activeName" accordion>
+      <van-collapse-item title="首页介绍" name="1" @click="scrollToTop">
+        <div class="text">
+          <p>首页，查看当前故障报修数量、个人信息、公告以及更多。</p>
+          <img src="@/assets/help/help1.jpg" width="250px" alt="" /><br />
+          <nut-divider dashed />
+        </div>
+      </van-collapse-item>
+      <van-collapse-item title="如何接单" name="2" @click="scrollToTop">
+        <div class="text">
+          <p>点击按钮进来，点击“抢单”为接手处理此故障订单。</p>
+          <img src="@/assets/help/help2.1.png" width="250px" alt="" /><br />
+          <img src="@/assets/help/help2.jpg" width="250px" alt="" /><br />
+          <nut-divider dashed />
+        </div>
+      </van-collapse-item>
+      <van-collapse-item title="接单后的各项操作" name="3" @click="scrollToTop">
+        <div class="text">
+          <p>点击上方“维修中”可以查看已接单</p>
+          <img src="@/assets/help/help3.jpg" width="250px" alt="" />
+          <img src="@/assets/help/help4.jpg" width="250px" alt="" /><br />
+          <nut-divider dashed />
+          <p>这里是自己接的单。维修完成后点击黄字”维修中“结束维修单。</p>
+          <img src="@/assets/help/help5.jpg" width="250px" alt="" /><br />
+          <nut-divider dashed />
+        </div>
+      </van-collapse-item>
+      <van-collapse-item
+        title="拆机维修流程（特殊）"
+        name="4"
+        @click="scrollToTop"
+      >
+        <div class="text">
+          <p>
+            如果有“拆机”单在修单前，需要点击“签订协议”，同时需要给笔记本细节进行拍照。机主签完名之后记得自己截图保存<br />
+            <span style="color: red">
+              如若自己没有保存照片及截图，则后期纠纷社员自己承担
+            </span>
+          </p>
+          <img src="@/assets/help/help6.jpg" width="250px" alt="" />
+          <img src="@/assets/help/help7.jpg" width="250px" alt="" />
+          <br />
+          <nut-divider dashed />
+          <p>点击此处对拆机过程的图片进行上传<br /></p>
+          <img src="@/assets/help/help8.jpg" width="250px" alt="" /><br />
+          <nut-divider dashed />
+          <p>在此处选择要上传的订单<br /></p>
+          <img src="@/assets/help/help9.jpg" width="250px" alt="" /><br />
+          <nut-divider dashed />
+          <p>按照所描述的规则进行上传填写<br /></p>
+          <img src="@/assets/help/help10.jpg" width="250px" alt="" /><br />
+          <nut-divider dashed />
+        </div>
+      </van-collapse-item>
+      <van-collapse-item title="个人中心" name="5" @click="scrollToTop">
+        <div class="text">
+          <p>个人中心，可查看历史处理记录，修改登陆密码等等。</p>
+          <img src="@/assets/help/help11.jpg" width="250px" alt="" /><br />
+          <nut-divider dashed />
+          <p>*为了账号安全，退出前务必点击“退出”按钮，退出此次登陆状态。</p>
+        </div>
+      </van-collapse-item>
+    </van-collapse>
+    <p style="width: 90%; margin: 20px auto; text-align: center; color: red">
+      此帮助页面由23届隽隽师兄编写
     </p>
-    <img src="@/assets/help/help6.jpg" width="250px" alt="" />
-    <img src="@/assets/help/help7.jpg" width="250px" alt="" />
-    <br />
-    <nut-divider dashed />
-    <p>5.2 点击此处对拆机过程的图片进行上传<br /></p>
-    <img src="@/assets/help/help8.jpg" width="250px" alt="" /><br />
-    <nut-divider dashed />
-    <p>5.3 在此处选择要上传的订单<br /></p>
-    <img src="@/assets/help/help9.jpg" width="250px" alt="" /><br />
-    <nut-divider dashed />
-    <p>5.4 按照所描述的规则进行上传填写<br /></p>
-    <img src="@/assets/help/help10.jpg" width="250px" alt="" /><br />
-    <nut-divider dashed />
-    <p>6.个人中心，可查看历史处理记录，修改登陆密码等等。</p>
-    <img src="@/assets/help/help11.jpg" width="250px" alt="" /><br />
-    <nut-divider dashed />
-    <p>*为了账号安全，退出前务必点击“退出”按钮，退出此次登陆状态。</p>
-    <p style="margin: 0; color: red">此帮助页面由23届隽隽师兄编写</p>
     <nut-divider class="foot-line" />
-    <p style="margin: 0">
+    <p style="width: 90%; margin: 0 auto; text-align: center">
       如果觉得此系统不完善，或者是想继续改进，需要源码，请联系我。微信：L18218644665。
     </p>
-    <p style="text-align: right">—— 22届师兄</p>
+    <p style="text-align: right; width: 90%; margin: 0 auto">—— 22届师兄</p>
   </div>
 </template>
 
 <style scoped>
 p {
-  color: #a0a0a0;
+  color: #666366;
   display: block;
   margin-block-start: 1em;
   margin-block-end: 1em;
@@ -78,12 +115,16 @@ p {
   margin-inline-end: 0;
 }
 .helpPage {
-  width: 80%;
   margin: 0 auto;
-  text-align: center;
-  img {
-    border: 1px solid #7a7374;
-    border-radius: 10px;
+  .text {
+    text-align: center;
+    width: 95%;
+    margin: 0 auto;
+    font-size: 16px;
+    img {
+      border: 1px solid #7a7374;
+      border-radius: 10px;
+    }
   }
 }
 .foot-line {
