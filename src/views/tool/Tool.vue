0fbc4bd15ff5688b83bbb1417bd094337ec70cc1
gastronome <1305573134@qq.com>
2024-10-21 21:47:04 +0800

    优化上传

--- src/views/tool/Tool.vue
+++ src/views/tool/Tool.vue
@@ -5,97 +5,8 @@ import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const list = [
-  {
-    name: 'QQ',
-    url: 'https://im.qq.com/index/#downloadAnchor',
-    photo: 'https://qzonestyle.gtimg.cn/qzone/qzact/act/external/tiqq/logo.png'
-  },
-  {
-    name: '微信',
-    url: 'https://pc.weixin.qq.com/',
-    photo:
-      'https://res.wx.qq.com/t/wx_fed/base/portal/windows/res/assets/logo.ca85d20a32.svg'
-  },
-  {
-    name: '飞书',
-    url: 'https://www.feishu.cn/download',
-    photo:
-      'https://p1-hera.feishucdn.com/tos-cn-i-jbbdkfciu3/84a9f036fe2b44f99b899fff4beeb963~tplv-jbbdkfciu3-image:0:0.image'
-  },
-  {
-    name: '钉钉',
-    url: 'https://page.dingtalk.com/wow/z/dingtalk/simple/ddhomedownload#/',
-    photo:
-      'https://gw.alicdn.com/imgextra/i3/O1CN01eMicSg1GVD4uXMWGv_!!6000000000627-73-tps-32-32.ico'
-  },
-  {
-    name: 'Todesk',
-    url: 'https://www.todesk.com/download.html',
-    photo: 'https://www.todesk.com/dist/img/734933e.svg'
-  },
-  {
-    name: 'steam',
-    url: 'https://store.steampowered.com/about/',
-    photo: 'https://store.steampowered.com/favicon.ico'
-  },
-  {
-    name: 'chrome',
-    url: 'https://www.google.cn/intl/zh-CN/chrome/',
-    photo: 'https://www.google.cn/chrome/static/images/chrome-logo-m100.svg'
-  },
-  {
-    name: 'WeGame',
-    url: 'https://www.wegame.com.cn/client/',
-    photo:
-      'https://wegame.gtimg.com/g.55555-r.c4663/wegame-home/wegame-h5-share.f036f35a.jpg'
-  }
-]
-const list1 = [
-  {
-    name: '图吧工具箱',
-    url: 'https://www.tbtool.cn/',
-    photo: 'https://www.tbtool.cn/resource/icon.ico'
-  },
-  {
-    name: 'Nvidia驱动',
-    url: 'https://www.nvidia.cn/drivers/lookup/',
-    photo: 'https://www.nvidia.cn/favicon.ico'
-  },
-  {
-    name: 'AMD驱动',
-    url: 'https://www.amd.com/zh-cn/support/download/drivers.html',
-    photo: 'https://www.amd.com/content/dam/code/images/favicon/favicon.ico'
-  }
-]
-const list2 = [
-  {
-    name: 'VsCode',
-    url: 'https://code.visualstudio.com/docs/?dv=win64user',
-    photo: 'https://code.visualstudio.com/favicon.ico'
-  },
-  {
-    name: 'sublimetext',
-    url: 'https://www.sublimetext.com/',
-    photo: 'https://www.sublimehq.com/images/sublime_text.png'
-  },
-  {
-    name: 'IDEA',
-    url: 'https://www.jetbrains.com/idea/download/#section=windows',
-    photo: 'https://www.jetbrains.com/icon.svg?r=1234'
-  },
-  {
-    name: 'eclipse',
-    url: 'https://www.eclipse.org/downloads/',
-    photo:
-      'https://www.eclipse.org/downloads/assets/public/images/logo-eclipse.png'
-  },
-  {
-    name: 'nodejs',
-    url: 'https://nodejs.cn/download/',
-    photo: 'https://img.nodejs.cn/favicon.png'
-  }
-]
+const appList = window.config.appList
+
 const back = () => {
   router.go(-1)
 }
@@ -115,9 +26,9 @@ const back = () => {
       >报修</a
     >，让我们帮助你！
   </div>
-  <ToolShow :app-list="list" title="日常软件"></ToolShow>
-  <ToolShow :app-list="list1" title="装机工具"></ToolShow>
-  <ToolShow :app-list="list2" title="编程开发"></ToolShow>
+  <div v-for="(app, index) in appList" :key="index">
+    <ToolShow :app-list="app.list" :title="app.title"></ToolShow>
+  </div>
   <nut-divider dashed />
   <p style="font-size: 11px; width: 80%; margin: 10px auto; color: #939393">
     &nbsp;&nbsp;&nbsp;&nbsp; 本站所发布的全部内容源于互联网搬运，
