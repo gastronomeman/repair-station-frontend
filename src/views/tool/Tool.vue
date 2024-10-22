89b7cb57d41576824d03e169964d9c0f2ba68293
gastronome <1305573134@qq.com>
2024-10-22 15:56:31 +0800

    优化工具ui

--- src/views/tool/Tool.vue
+++ src/views/tool/Tool.vue
@@ -334,24 +334,26 @@ const copyToClipboard = () => {
       </template>
     </nut-navbar>
   </nut-sticky>
-  <div class="title">
-    **此网页的工具是为了帮助同学们更好的找到各个软件的官网，用作学习之用，如遇不会安装可以点击<a
-      href="/"
-      >报修</a
-    >，让我们帮助你！<br />
-    <p @click="copyToClipboard" :style="{ color: textColor }">
-      点击此处，复制网址<br />（复制网址到电脑浏览更佳哦）
+  <div style="max-width: 800px; margin: 0 auto">
+    <div class="title">
+      **此网页的工具是为了帮助同学们更好的找到各个软件的官网，用作学习之用，如遇不会安装可以点击<a
+        href="/"
+        >报修</a
+      >，让我们帮助你！<br />
+      <p @click="copyToClipboard" :style="{ color: textColor }">
+        点击此处，复制网址<br />（复制网址到电脑浏览更佳哦）
+      </p>
+    </div>
+    <div v-for="(app, index) in appList" :key="index">
+      <ToolShow :app-list="app.list" :title="app.title"></ToolShow>
+    </div>
+    <nut-divider dashed />
+    <p style="font-size: 11px; width: 80%; margin: 10px auto; color: #939393">
+      &nbsp;&nbsp;&nbsp;&nbsp; 本站所发布的全部内容源于互联网搬运，
+      仅限于小范围内传播学习和文献参考， 请在下载后24小时内删除，
+      如果有侵权之处请第一时间联系我们删除。敬请谅解! E-mail：1305573134@qq.com
     </p>
   </div>
-  <div v-for="(app, index) in appList" :key="index">
-    <ToolShow :app-list="app.list" :title="app.title"></ToolShow>
-  </div>
-  <nut-divider dashed />
-  <p style="font-size: 11px; width: 80%; margin: 10px auto; color: #939393">
-    &nbsp;&nbsp;&nbsp;&nbsp; 本站所发布的全部内容源于互联网搬运，
-    仅限于小范围内传播学习和文献参考， 请在下载后24小时内删除，
-    如果有侵权之处请第一时间联系我们删除。敬请谅解! E-mail：1305573134@qq.com
-  </p>
   <Footer></Footer>
   <br />
 </template>
