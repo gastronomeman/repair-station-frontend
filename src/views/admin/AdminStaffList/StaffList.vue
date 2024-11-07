d92fda4cded4f4f27c2742c18c5db81071b3e963
gastronome <1305573134@qq.com>
2024-11-07 22:15:21 +0800

    添加新的模块

--- src/views/admin/AdminStaffList/StaffList.vue
+++ src/views/admin/AdminStaffList/StaffList.vue
@@ -8,6 +8,7 @@ import { baseURL } from '@/utils/request.js'
 const router = useRouter()
 const adminState = useAdminState()
 
+const tabs = ref('1')
 const loading = ref(false)
 const value1 = ref(adminState.startTime)
 const value2 = ref(adminState.endTime)
@@ -42,78 +43,121 @@ const getCountCsv = async () => {
     baseURL +
     `/staff/count-csv?startTime=${value1.value}&endTime=${value2.value}`
 }
+
+const value = ref([])
+const onChange = (val) => {
+  console.log(val)
+}
 </script>
 
 <template>
-  <nut-navbar title="维修统计"></nut-navbar>
-  <div class="date-picker">
-    <span class="demonstration">开始日期：</span>
-    <el-date-picker
-      v-model="value1"
-      type="datetime"
-      :editable="false"
-      placeholder="开始日期"
-      value-format="YYYY-MM-DD HH:mm:ss"
-    />
-    <div class="br"></div>
-    <span class="demonstration">结束日期：</span>
-    <el-date-picker
-      v-model="value2"
-      :editable="false"
-      type="datetime"
-      placeholder="结束日期"
-      value-format="YYYY-MM-DD HH:mm:ss"
-    />
-    <div class="list-button">
-      <nut-button type="info" @click="getCountCsv">导出数据</nut-button>
-      &nbsp;&nbsp;
-      <nut-button type="info" @click="resetList">重置</nut-button>
-      &nbsp;&nbsp;
-      <nut-button type="info" @click="getOrdersList">查找</nut-button>
-    </div>
-    <nut-divider dashed></nut-divider>
-  </div>
-  <div
-    v-for="(l, index) in leaderboard"
-    :key="index"
-    class="leaderboard"
-    v-loading="loading"
-  >
-    <nut-navbar @click="toStaffOrders(l.id)" style="cursor: pointer">
-      <template #left>
-        <span class="nav-title">{{ l.name }}</span>
-      </template>
-      <template #right>
-        <span class="nav-title">{{ l.orderCount }}单</span>
-      </template>
-    </nut-navbar>
-    <nut-divider dashed />
-  </div>
+  <nut-navbar title="维修-摆摊"></nut-navbar>
+  <nut-tabs v-model="tabs" type="smile">
+    <nut-tab-pane title="维修统计" pane-key="1">
+      <div class="date-picker">
+        <span class="demonstration">开始日期：</span>
+        <el-date-picker
+          v-model="value1"
+          type="datetime"
+          :editable="false"
+          placeholder="开始日期"
+          value-format="YYYY-MM-DD HH:mm:ss"
+        />
+        <div class="br"></div>
+        <span class="demonstration">结束日期：</span>
+        <el-date-picker
+          v-model="value2"
+          :editable="false"
+          type="datetime"
+          placeholder="结束日期"
+          value-format="YYYY-MM-DD HH:mm:ss"
+        />
+        <div class="list-button">
+          <nut-button type="info" @click="getCountCsv">导出数据</nut-button>
+          &nbsp;&nbsp;
+          <nut-button type="info" @click="resetList">重置</nut-button>
+          &nbsp;&nbsp;
+          <nut-button type="info" @click="getOrdersList">查找</nut-button>
+        </div>
+        <nut-divider dashed></nut-divider>
+      </div>
+      <div
+        v-for="(l, index) in leaderboard"
+        :key="index"
+        class="leaderboard"
+        v-loading="loading"
+      >
+        <nut-navbar @click="toStaffOrders(l.id)" style="cursor: pointer">
+          <template #left>
+            <span class="nav-title">{{ l.name }}</span>
+          </template>
+          <template #right>
+            <span class="nav-title">{{ l.orderCount }}单</span>
+          </template>
+        </nut-navbar>
+        <nut-divider dashed />
+      </div>
+    </nut-tab-pane>
+    <nut-tab-pane title="摆摊发布" pane-key="2">
+      <el-empty style="background: #f0f0f0">
+        <template #description>
+          <p>
+            别着急师兄在写了<br />
+            o(╥﹏╥)o<br />
+            <a href="https://gitee.com/gastronome-0_0/repair-station-app"
+              >https://gitee.com/gastronome-0_0/repair-station-app
+            </a>
+          </p>
+        </template>
+      </el-empty>
+      <div class="task">
+        <div>
+          <nut-tag v-for="d in value" :key="d.getTime()" type="success">
+            {{ `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}` }}
+          </nut-tag>
+        </div>
+        <nut-calendar-card
+          v-model="value"
+          type="multiple"
+          @change="onChange"
+        ></nut-calendar-card>
+      </div>
+    </nut-tab-pane>
+  </nut-tabs>
 </template>
 
 <style scoped>
-.date-picker {
-  width: 80%;
-  margin: 20px auto;
-  text-align: center;
-  .br {
-    margin: 10px 0;
+.nut-tab-pane {
+  background-color: #f0f0f0;
+  padding: 0 !important;
+  .date-picker {
+    width: 80%;
+    margin: 20px auto;
+    text-align: center;
+    .br {
+      margin: 10px 0;
+    }
+    .list-button {
+      margin: 8px 0;
+    }
+    .nut-divider {
+      margin: 5px 0;
+    }
   }
-  .list-button {
-    margin: 8px 0;
+  .leaderboard {
+    width: 80%;
+    margin: 0 auto;
+    span {
+      color: black;
+    }
+    .nut-divider {
+      margin: 0;
+    }
   }
-  .nut-divider {
-    margin: 5px 0;
-  }
-}
-.leaderboard {
-  width: 80%;
-  margin: 0 auto;
-  span {
-    color: black;
-  }
-  .nut-divider {
-    margin: 0;
+
+  .task {
+    max-width: 500px;
+    margin: 20px auto;
   }
 }
 </style>
--- src/views/staff/StaffAcceptedOrders.vue
+++ src/views/staff/StaffAcceptedOrders.vue
@@ -17,31 +17,54 @@ const getOrdersList = async () => {
 getOrdersList()
 
 const refreshOrdersList = async () => getOrdersList()
+
+const tabs = ref('1')
 </script>
 
 <template>
   <nut-navbar title="任务"></nut-navbar>
-  <div class="loading" v-loading="loading">
-    <div v-for="order in ordersList" :key="order.id">
-      <div class="br">&nbsp;</div>
-      <orders-task-item
-        @refresh="refreshOrdersList"
-        :order="order"
-      ></orders-task-item>
-    </div>
-    <el-empty style="background: #f0f0f0" v-if="ordersList.length === 0">
-      <template #description>
-        <p>
-          修单其实并不难哦，快去尝试一下吧！<br />遇到困难可以去寻求师兄师姐的帮助哦！<br />
-          (*๓´╰╯`๓)♡
-        </p>
-      </template>
-    </el-empty>
-  </div>
+  <nut-tabs v-model="tabs">
+    <nut-tab-pane title="日常维修" pane-key="1">
+      <div class="loading" v-loading="loading">
+        <div v-for="order in ordersList" :key="order.id">
+          <div class="br">&nbsp;</div>
+          <orders-task-item
+            @refresh="refreshOrdersList"
+            :order="order"
+          ></orders-task-item>
+        </div>
+        <el-empty style="background: #f0f0f0" v-if="ordersList.length === 0">
+          <template #description>
+            <p>
+              修单其实并不难哦，快去尝试一下吧！<br />遇到困难可以去寻求师兄师姐的帮助哦！<br />
+              (*๓´╰╯`๓)♡
+            </p>
+          </template>
+        </el-empty>
+      </div>
+    </nut-tab-pane>
+    <nut-tab-pane title="摆摊" pane-key="2">
+      <el-empty style="background: #f0f0f0">
+        <template #description>
+          <p>
+            别着急师兄在写了<br />
+            o(╥﹏╥)o<br />
+            <a href="https://gitee.com/gastronome-0_0/repair-station-app"
+              >https://gitee.com/gastronome-0_0/repair-station-app
+            </a>
+          </p>
+        </template>
+      </el-empty>
+    </nut-tab-pane>
+  </nut-tabs>
 </template>
 
 <style scoped>
-.loading {
-  margin-bottom: 50px;
+.nut-tab-pane {
+  background-color: #f0f0f0;
+  padding: 0 !important;
+  .loading {
+    margin-bottom: 50px;
+  }
 }
 </style>
