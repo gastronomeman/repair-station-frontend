4fad9323c49f3b26d5f40af9982af2d5942d2333
gastronome <1305573134@qq.com>
2024-11-12 01:23:55 +0800

    添加答题模块

--- src/views/admin/exam/ExamSub.vue
+++ src/views/admin/exam/ExamSub.vue
@@ -1,5 +1,31 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
 
-<template>1</template>
+const sub = ref('')
+</script>
 
-<style scoped></style>
+<template>
+  <div class="search">
+    <el-input
+      v-model="sub"
+      clearable
+      style="width: 150px"
+      placeholder="请输入题目以查询"
+    />&nbsp;
+    <nut-button type="info" size="small">查询</nut-button>
+    &nbsp;
+    <nut-button type="info" size="small">重置</nut-button>
+  </div>
+  <div style="text-align: center">
+    <nut-button type="info" size="small">批量添加</nut-button>&nbsp;
+    <nut-button type="info" size="small">单个添加</nut-button>
+  </div>
+</template>
+
+<style scoped>
+.search {
+  width: 90%;
+  margin: 8px auto 15px;
+  text-align: center;
+}
+</style>
